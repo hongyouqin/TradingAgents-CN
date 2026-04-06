661d13def71f8d53fb1b0def40131523003d0120
qinhy <yang.qq123@163.com>
2026-04-06 20:24:19 +0800

    解决sex字段问题

--- app/models/user.py
+++ app/models/user.py
@@ -105,7 +105,7 @@ class User(BaseModel):
     username: str = Field(..., min_length=3, max_length=50)
     nickname: Optional[str] = None
     avatar: Optional[str] = None
-    sex: Optional[str] = None
+    sex: Optional[int] = None
     city: Optional[str] = None
     province: Optional[str] = None
     # email: str = Field(..., pattern=r'^[^@]+@[^@]+\.[^@]+$')
